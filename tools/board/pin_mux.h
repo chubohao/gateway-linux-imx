cdf09cd64759f2989dad7d54f51d8542a05d5220
chubohao <bohao.chu@qq.com>
2022-09-13 00:07:19 +0200

    add tools

---
+++ tools/board/pin_mux.h
@@ -0,0 +1,20 @@
+#ifndef _PIN_MUX_H_
+#define _PIN_MUX_H_
+
+#include "board.h"
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+/*!
+ * @brief Configures pin routing and optionally pin electrical features.
+ *
+ */
+void BOARD_InitPins(void);
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif /* _PIN_MUX_H_ */
